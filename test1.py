71035d23d40a69e5df2cd19f127dada11928848e
meteor-python <c_zhicheng_9@163.com>
2023-04-02 13:39:00 +0800

    Update test1.py
    fix

--- test1.py
+++ test1.py
@@ -5,6 +5,8 @@ import untitled1
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 if __name__ == '__main__':
+    a = 2
+    print("hello %d\n"%a)
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = untitled1.Ui_MainWindow()
