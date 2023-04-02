c7660ca88bc4408572e7e6cb3a71076b71374feb
meteor-python <c_zhicheng_9@163.com>
2023-04-02 13:59:11 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,8 +5,11 @@ import untitled1
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 if __name__ == '__main__':
+<<<<<<< HEAD
     a = 6
     print("hello %d\n"%a)
+=======
+>>>>>>> parent of 71035d2 (Update test1.py)
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = untitled1.Ui_MainWindow()
