774ef6389a911e259a4c8023c77949ff0c86785b
meteor-python <c_zhicheng_9@163.com>
2023-04-02 14:01:03 +0800

    fix

--- test1.py
+++ test1.py
@@ -1,15 +1,13 @@
 print("Hello world")
 print("I love uestc")
+a = 3
+print("%d"%a)
 import sys
 import untitled1
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    a = 6
-    print("hello %d\n"%a)
-=======
->>>>>>> parent of 71035d2 (Update test1.py)
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = untitled1.Ui_MainWindow()
